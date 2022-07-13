87c9b75453805dcf88f4a14f7364a84aa209faf9
janek.wilkowski@gmail.com <janek.wilkowski@gmail.com>
2022-07-13 19:35:46 +0200

    xpbottle Event

---
+++ src/main/java/com/jano/plugin/listeners/XPBottleBreakListener.java
@@ -0,0 +1,16 @@
+package com.jano.plugin.listeners;
+
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ExpBottleEvent;
+
+public class XPBottleBreakListener implements Listener {
+
+    @EventHandler
+    public void onXPBottleBreak(ExpBottleEvent e){
+
+        Block block = e.getHitBlock();
+        block.breakNaturally();
+    }
+}
